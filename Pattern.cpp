9d3fcbc86109ceb12119017cc2e3da8ccf804782
Naman <sri.naman07@gmail.com>
2022-06-12 11:13:32 +0530

    Apni Kaksha DSA

---
+++ Pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+//Hollow Rectangle
+
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+    for(int i=1;i<=row;i++)
+    {
+        for(int j=1;j<=col;j++)
+        {
+            if(i==1 || i==row || j==col || j==1){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
---
+++ Pattern3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//Half Pyramid After 180' rotation
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(j<=n-i)
+            {
+                cout<<" ";
+            }
+            else
+            {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+   
+}
