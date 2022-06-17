55ad4424399aa56398da81aeea2a4819d68e1464
Naman <sri.naman07@gmail.com>
2022-06-17 17:30:25 +0530

    Patterns

--- Pattern11.cpp
+++ Pattern11.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 // Palindromic Pattern
 
+//         1 
+//       2 1 2
+//     3 2 1 2 3
+//   4 3 2 1 2 3 4
+// 5 4 3 2 1 2 3 4 5
+
 int main()
 {
     int n;
@@ -14,23 +20,42 @@ int main()
     {
         for (int j = 1; j <= n - i; j++)
         {
-            cout << " ";
+            cout << "  ";
         }
         int a = i;
         for (int j = 1; j <= i; j++)
         {
-            cout << a;
+            cout << a << " ";
             a--;
         }
         int b = 2;
         for (int j = 1; j <= i - 1; j++)
         {
-
-            cout << b;
+            cout << b << " ";
             b++;
         }
         cout << endl;
     }
 
     // Method 2
+
+    for (int i = 1; i <= n; i++)
+    {
+        int j;
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << "  ";
+        }
+        int a = i;
+        for (; j <= n; j++)
+        {
+            cout << a-- << " ";
+        }
+        int b = 2;
+        for (; j <= n + i - 1; j++)
+        {
+            cout << b++ << " ";
+        }
+        cout << endl;
+    }
 }
